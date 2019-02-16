86e0ff8c32025cdc93b50616fe361e45da9ad4ed
Yuhei Okazaki <okazaki@fusic.co.jp>
2019-02-16 22:46:06 +0900

    body encrypted

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,29 +1,21 @@
 class DiariesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_diary, only: [:show, :edit, :update, :destroy]
+  before_action :set_diaries
 
-  # GET /diaries
-  # GET /diaries.json
   def index
-    @diaries = current_user.diaries
   end
 
-  # GET /diaries/1
-  # GET /diaries/1.json
   def show
   end
 
-  # GET /diaries/new
   def new
     @diary = current_user.diaries.build(date: params[:date])
   end
 
-  # GET /diaries/1/edit
   def edit
   end
 
-  # POST /diaries
-  # POST /diaries.json
   def create
     @diary = current_user.diaries.build(diary_params)
     if @diary.save
@@ -33,8 +25,6 @@ class DiariesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /diaries/1
-  # PATCH/PUT /diaries/1.json
   def update
     if @diary.update(diary_params)
       redirect_to @diary, notice: 'Diary was successfully updated.'
@@ -43,21 +33,21 @@ class DiariesController < ApplicationController
     end
   end
 
-  # DELETE /diaries/1
-  # DELETE /diaries/1.json
   def destroy
     @diary.destroy
     redirect_to diaries_url, notice: 'Diary was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    def set_diaries
+      @diaries = current_user.diaries
+    end
+
     def set_diary
       @diary = current_user.diaries.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def diary_params
-      params.require(:diary).permit(:encrypted_body, :date, :user_id)
+      params.require(:diary).permit(:body, :date, :user_id)
     end
 end
--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,4 +1,22 @@
 class Diary < ApplicationRecord
+  attr_accessor :body
+
   validates :date, presence: true, uniqueness: true
-  validates :encrypted_body, presence: true
+  validates :body, presence: true
+
+  before_save do
+    crypt = ActiveSupport::MessageEncryptor.new(key)
+    self.encrypted_body = crypt.encrypt_and_sign(self.body)
+  end
+
+  after_find do
+    crypt = ActiveSupport::MessageEncryptor.new(key)
+    self.body = crypt.decrypt_and_verify(self.encrypted_body)
+  end
+
+  private
+
+  def key
+    Rails.application.secrets.secret_key_base.slice(0, 32)
+  end
 end
