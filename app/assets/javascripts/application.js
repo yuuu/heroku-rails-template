e5add2f2b2bb2bce24abe47a27c80b647654f715
Yuhei Okazaki <okazaki@fusic.co.jp>
2019-03-02 21:47:08 +0900

    メニューを整理

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,6 +36,13 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.dropdown-trigger')
+  var instances = M.Dropdown.init(elems, {});
+});
+
+document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.materialboxed');
   var instances = M.Materialbox.init(elems, {});
 });
+
+
